10277411a530dd115f09cdb538d98b1e20c4a92d
myoui <mypenguinmina@gmail.com>
2022-11-02 22:26:08 +0700

    :)

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -17,7 +17,7 @@ import {
 } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 
-const Item = ({ name, _icon }: { name: string; _icon: IconType }) => {
+const Icon = ({ name, _icon }: { name: string; _icon: IconType }) => {
   return (
     <div className="flex items-center space-x-2 rounded-md border border-white/20 bg-white/60 dark:bg-[#5f5555ad] p-3">
       <_icon className="h-6 w-6 text-neutral-600 dark:text-white/75" />
@@ -29,21 +29,21 @@ const Item = ({ name, _icon }: { name: string; _icon: IconType }) => {
 const Technologies = () => {
   return (
     <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-      <Item name="C++" _icon={SiCplusplus} />
-      <Item name="C#" _icon={SiCsharp} />
-      <Item name="php" _icon={SiPhp} />
-      <Item name="Java" _icon={SiJava} />
-      <Item name="HTML" _icon={SiHtml5} />
-      <Item name="CSS" _icon={SiCss3} />
-      <Item name="Javascript" _icon={SiJavascript} />
-      <Item name="Typescript" _icon={SiTypescript} />
-      <Item name="Tailwind CSS" _icon={SiTailwindcss} />
-      <Item name="MySQL" _icon={GrMysql} />
-      <Item name="MongoDB" _icon={SiMongodb} />
-      <Item name="Node.js" _icon={SiNodedotjs} />
-      <Item name="React.js" _icon={SiReact} />
-      <Item name="Next.js" _icon={SiNextdotjs} />
-      <Item name="Git" _icon={SiGit} />
+      <Icon name="C++" _icon={SiCplusplus} />
+      <Icon name="C#" _icon={SiCsharp} />
+      <Icon name="php" _icon={SiPhp} />
+      <Icon name="Java" _icon={SiJava} />
+      <Icon name="HTML" _icon={SiHtml5} />
+      <Icon name="CSS" _icon={SiCss3} />
+      <Icon name="Javascript" _icon={SiJavascript} />
+      <Icon name="Typescript" _icon={SiTypescript} />
+      <Icon name="Tailwind CSS" _icon={SiTailwindcss} />
+      <Icon name="MySQL" _icon={GrMysql} />
+      <Icon name="MongoDB" _icon={SiMongodb} />
+      <Icon name="Node.js" _icon={SiNodedotjs} />
+      <Icon name="React.js" _icon={SiReact} />
+      <Icon name="Next.js" _icon={SiNextdotjs} />
+      <Icon name="Git" _icon={SiGit} />
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,12 +17,17 @@ const Home: NextPage = () => {
 			>
 				<div className="space-y-4">
 					<h1 className="mt-36 font-bold text-4xl md:text-5xl">Hi, I'm Tinh</h1>
-					<p className="text-gray-800 dark:text-gray-300 leading-6 tracking-wide mb-12">
+					<p className="text-gray-800 dark:text-gray-300 leading-6 tracking-wide mb-1">
 						I'm {Math.floor(age)} years old and i am studying information technology at a university
-						in Ho Chi Minh city, I'm trying to learn everything to improve my skills
+						in Ho Chi Minh city, i'm trying to become a web developer
+					</p>
+					<h2 className="mt-36 font-bold text-4xl md:text-3xl">My hobbies</h2>
+					<p className="text-gray-800 dark:text-gray-300 leading-6 tracking-wide mb-12 items-start">
+						I like listening to music the most, coding, playing games, and sometimes i playing soccer outside,
+						i'm a bit interesting in editing short video on ig by after effect, u can find my edits <a href={"https://www.instagram.com/anotherdayily_/"} target="_blank" rel="noopener">here</a>
 					</p>
 				</div>
-
+				
 				{/* Technologies */}
 				<div className="space-y-4">
 					<h2 className="mt-36 text-4xl md:text-3xl">Technologies 💻</h2>
