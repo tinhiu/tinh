4f73613a7982e2949a71af28dd1d0172bd66db2c
myoui <mypenguinmina@gmail.com>
2023-01-27 23:29:01 +0700

    js -> ts

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,24 +1,22 @@
-const Project = ({props}: {props: any}) => {
+const Project = ({url, title, description}: {url: string, title: string, description: string}) => {
   return (
     <div className="group relative flex">
       <div className="absolute -inset-0.5 rounded-lg opacity-0 transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
       <div className="absolute -inset-0.5 rounded-lg opacity-0 blur-sm transition-all duration-700 ease-in-out group-hover:opacity-100"></div>
       <a
-        href={props.url}
+        href={url}
         target="_blank"
         rel="noopener noreferrer"
         className="flex w-full"
       >
         <button className="relative w-full rounded-lg border border-gray-700 dark:border-gray-100 bg-transparent px-7 py-6 transition-all duration-700 ease-in-out group-hover:bg-slate-400">
           <span className="mb-3 block text-left font-semibold text-black dark:text-white">
-            {props.title}
+            {title}
           </span>
           <span className="block text-left text-sm text-gray-700 dark:text-white">
-            {props.description}
+            {description}
           </span>
-          <span className="mt-5 flex items-center space-x-5 text-black dark:text-white">
-            {props.icons}
-          </span>
+         
         </button>
       </a>
     </div>
