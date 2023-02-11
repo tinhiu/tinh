3a6b593824579f21e01f930203f89250471db709
myoui <mypenguinmina@gmail.com>
2023-02-11 22:36:57 +0700

    undefined

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -6,7 +6,7 @@ import { SiSpotify } from 'react-icons/si';
 export const DISCORD_ID = '885439540268003338';
 const Song = () => {
 
-	const user = useLanyardWS(DISCORD_ID)!;
+	const user = useLanyardWS(DISCORD_ID);
 	
 	if (!user || !user.spotify) {
 		return (
@@ -46,18 +46,18 @@ const Song = () => {
 					<div className="w-full h-[6rem] flex flex-row items-center justify-start">
 						<div className="hover:scale-125 transition duration-500 hover:ease-out">
 							<img
-								src={user.spotify.album_art_url}
+								src={user.spotify.album_art_url || undefined}
 								className="w-[4.5rem] h-[4.5rem] rounded-md mr-4 pointer-events-none bg-cover "
 								alt={user.spotify.album}
 							/>
 						</div>
 						<div className="w-56 h-full flex flex-col items-start justify-center">
 							<a
-								href={`https://open.spotify.com/track/${user.spotify.track_id}`}
+								href={`https://open.spotify.com/track/${user.spotify.track_id || undefined}`}
 								target="_blank"
 								rel="noreferrer"
 								className="w-full font-medium text-black-900 dark:text-[#e1eafd] hover:underline truncate"
-								title={user.spotify.song}
+								title={user.spotify.song || undefined}
 							>
 								{user.spotify.song}
 							</a>
