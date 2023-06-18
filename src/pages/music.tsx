bc93fb317fef57d536a410179e94c515b2184a6d
myoui <mypenguinmina@gmail.com>
2023-06-18 12:34:20 +0700

    ?

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -57,7 +57,7 @@ export default function MusicPage({
 	userLanyard,
 	randomLastFMTrack,
 }: Props) {
-	const image = user.images[0].url;
+	const image = user?.images[0].url;
 
 	//console.log(JSON.stringify(userLanyard, null, 4));
 
