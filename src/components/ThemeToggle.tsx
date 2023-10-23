cf3683c8e51e3ad6f97f3dd357db061d5758b483
Myoui <mypenguinmina@gmail.com>
2023-10-24 00:29:28 +0700

    add sun-and-moon

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { FiSun, FiMoon } from "react-icons/fi";
 
 const ThemeToggle = () => {
     const [theme, setTheme] = useState<string>("light");
@@ -17,9 +16,9 @@ const ThemeToggle = () => {
 
     const changeTheme = (theme: string) => {
         let newTheme = theme === "light" ? "dark" : "light";
-
         localStorage.setItem("theme", newTheme);
         setTheme(newTheme);
+        document.querySelector('#theme-toggle')?.setAttribute('aria-label', newTheme)
         newTheme === "light"
             ? document.querySelector("html")?.classList.remove("dark")
             : document.querySelector("html")?.classList.add("dark");
@@ -27,11 +26,26 @@ const ThemeToggle = () => {
 
     return (
         <button
-            className="cursor-pointer rounded-full p-2 hover:border-violet-700/60 hover:bg-black/5 dark:hover:bg-white/5"
-            onClick={() => changeTheme(theme)}
-        >
-            {theme === "light" && <FiSun className="h-6 w-6 text-stone-600" />}
-            {theme === "dark" && <FiMoon className="h-6 w-6 text-white/80" />}
+            className="theme-toggle cursor-pointer rounded-full p-2 hover:border-violet-700/60 hover:bg-black/5 dark:hover:bg-white/5"
+            id="theme-toggle" title="Toggles light & dark" aria-label="auto" aria-live="polite"
+            onClick={() => changeTheme(theme)}>
+            <svg className="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
+                <mask className="moon" id="moon-mask">
+                    <rect x="0" y="0" width="100%" height="100%" fill="white" />
+                    <circle cx="24" cy="10" r="6" fill="black" />
+                </mask>
+                <circle className="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
+                <g className="sun-beams" stroke="currentColor">
+                    <line x1="12" y1="1" x2="12" y2="3" />
+                    <line x1="12" y1="21" x2="12" y2="23" />
+                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                    <line x1="1" y1="12" x2="3" y2="12" />
+                    <line x1="21" y1="12" x2="23" y2="12" />
+                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+                </g>
+            </svg>
         </button>
     );
 };
